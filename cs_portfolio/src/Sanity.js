48e5e1ec60a55215a1f7df32d66e6821349c5aa4
vedp2003 <vedp2003@gmail.com>
2025-01-03 00:50:14 -0500

    Prepare to Deploy

--- cs_portfolio/src/Sanity.js
+++ cs_portfolio/src/Sanity.js
@@ -1,7 +1,9 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-  projectId: 'om691cq7', // My project ID from sanity.config.ts
-  dataset: 'production',      
-  useCdn: true,              
+  projectId: 'om691cq7', // project ID from sanity.config.ts
+  dataset: 'production',
+  apiVersion: '2023-01-01', 
+  token: process.env.REACT_APP_SANITY_TOKEN, 
+  useCdn: false, 
 });
