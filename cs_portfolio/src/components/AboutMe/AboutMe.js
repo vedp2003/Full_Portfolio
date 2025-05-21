55766df7c14db497768e09e84bfc8e1859464568
vedp2003 <vedp2003@gmail.com>
2025-05-21 15:31:16 -0400

    Updated AboutMe

--- cs_portfolio/src/components/AboutMe/AboutMe.js
+++ cs_portfolio/src/components/AboutMe/AboutMe.js
@@ -54,13 +54,12 @@ const AboutMe = () => {
         <div className="about-me-center">
           <h1 className="about-me-title">About Me</h1>
           <p className="about-me-description">
-            Hi! This is <strong>Ved Patel</strong>. I am a senior at Rutgers University, pursuing a B.S. in Computer Science and Data Science. I'm a passionate and
-            dedicated developer with a strong interest in web development, mobile development, and machine learning. I have honed my skills through
-            various hands-on projects and am constantly eager to learn new
+            Hi! This is <strong>Ved Patel</strong>. I am a recent graduate from Rutgers University, with a B.S. in Computer Science and Data Science, along with having completed a minor in Business Administration. I'm a passionate and
+            dedicated developer with a strong interest in web development, mobile development, data analysis, and machine learning. I have honed my skills through various hands-on projects and am constantly eager to learn new
             technologies.
           </p>
           <p className="about-me-description">
-            I love working collaboratively towards building new solutions to real-life and practical issues, such as being able to create a robust machine learning model to assist with predicting healthcare/sports trends. 
+            I love working collaboratively towards building new solutions to real-life and practical issues, such as being able to create a robust machine learning model to assist with predicting healthcare/business/sports trends. I'm currently pursuing certifications in both AWS Cloud Practitioner and Microsoft Azure Fundamentals.
           </p>
         </div>
         <div className="about-me-right">
